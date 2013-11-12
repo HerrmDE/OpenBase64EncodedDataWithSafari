4eb7712e22d57709338da04779d71086e4afdbbf
Christian Herrmann <chris@herrm.de>
2013-11-12 22:13:37 +0100

    Replaced category with CHEDataURLHandler class

--- CHEDataURLHandler.m
+++ CHEDataURLHandler.m
@@ -1,50 +1,52 @@
 //
-//  UIApplication+DataURL.m
-//  OpenBase64EncodedDataURLWithSafari
+//  CHEDataURLHandler.m
+//  OpenDataURLSample
 //
-//  Created by Christian Herrmann on 19.10.13.
-//  Copyright (c) 2013 herrm.de. All rights reserved.
+//  Created by Christian Herrmann on 12.11.13.
+//  Copyright (c) 2013 HerrmDE. All rights reserved.
 //
 
-#import "UIApplication+DataURL.h"
+#import "CHEDataURLHandler.h"
 #import "HTTPServer.h"
 #import "DDTTYLogger.h"
 #import "DDLog.h"
 
-static const int ddLogLevel = LOG_LEVEL_VERBOSE;
-static const int chePort = 33654;
-static const HTTPServer *cheHTTPServer;
 
-@implementation UIApplication (DataURL)
+@implementation CHEDataURLHandler
+{
+	HTTPServer *httpServer;
+}
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 - (BOOL)openBase64EncodedContent:(NSString *)contentString
 {
 	[DDLog addLogger:[DDTTYLogger sharedInstance]];
 	
-	cheHTTPServer = [[HTTPServer alloc] init];
-	[cheHTTPServer setType:@"_http._tcp."];
-	[cheHTTPServer setPort:CHEPort];
+	httpServer = [[HTTPServer alloc] init];
+	[httpServer setType:@"_http._tcp."];
+	[httpServer setPort:33664];
 	
-	//Setting document root to documents folder
+	//Creating document root at documents folder
 	NSString *documentRoot = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"] stringByAppendingPathComponent:@"Web"];
 	[[NSFileManager defaultManager] createDirectoryAtPath:documentRoot withIntermediateDirectories:YES attributes:nil error:nil];
-	[cheHTTPServer setDocumentRoot:documentRoot];
+	[httpServer setDocumentRoot:documentRoot];
 	
-	//Creating index.html which redirects to the data url
-	NSString* fileAtPath = [cheHTTPServer.documentRoot stringByAppendingPathComponent:@"index.html"];
+	//Creating index.html with redirect to data url
+	NSString* fileAtPath = [httpServer.documentRoot stringByAppendingPathComponent:@"index.html"];
 	[[NSFileManager defaultManager] createFileAtPath:fileAtPath contents:nil attributes:nil];
 	
 	NSString *indexHTMLString = [NSString stringWithFormat:@"<head><meta http-equiv=\"refresh\" content=\"0; URL=data:text/html;charset=UTF-8;base64,%@\"></head>", contentString];
 	[[indexHTMLString dataUsingEncoding:NSUTF8StringEncoding] writeToFile:fileAtPath atomically:NO];
 	
 	NSError *error;
-	if (![cheHTTPServer start:&error])
+	if (![httpServer start:&error])
 	{
 		DDLogError(@"Error starting HTTP Server: %@", error);
 	}
 	
 	//Starting web server
-    NSString* server = [NSString stringWithFormat:@"http://localhost:%hu", cheHTTPServer.listeningPort];
+    NSString* server = [NSString stringWithFormat:@"http://localhost:%hu", httpServer.listeningPort];
     NSURL *myURL = [NSURL URLWithString: [NSString stringWithFormat: @"%@", server]];
     [[UIApplication sharedApplication] openURL:myURL];
 	
